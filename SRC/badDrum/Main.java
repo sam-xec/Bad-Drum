c704996228acb9daab4cefbd34659bd67f31d8b6
sam-xec <samishaikh20041507@gmail.com>
2026-04-09 15:47:07 +0100

    SoundManager code has been finished

--- SRC/badDrum/Main.java
+++ SRC/badDrum/Main.java
@@ -1,6 +1,9 @@
 package baddrum;
 
+import baddrum.sound.SoundManager;
+
 public class Main{
 public static void main(String[] args){
+    SoundManager.playSound("495662__lilmati__hi-hat.wav");
     System.out.println("Drum project started ");
 }}
--- SRC/badDrum/sound/soundManager.java
+++ SRC/badDrum/sound/soundManager.java
@@ -1,11 +1,8 @@
 package baddrum.sound;
 //All the sound will be produced in this class
 
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+
+import javax.sound.sampled.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,20 +11,16 @@ import java.util.Map;
 
 public class SoundManager {
 
-private static final String ASSET_PATH = "asset/";
+ private static final String ASSET_PATH = "assets/sounds/";
     private static final Map<String, Clip> clipCache = new HashMap<>();
 
-    /**
-     * Plays a sound from the asset folder.
-     * @param fileName Name of the .wav file (e.g. "snare.wav")
-     */
     public static void playSound(String fileName) {
         new Thread(() -> {
             try {
                 Clip clip = getClip(fileName);
 
                 if (clip.isRunning()) {
-                    clip.stop();          // Allows retriggering
+                    clip.stop();
                 }
 
                 clip.setFramePosition(0);
@@ -35,13 +28,11 @@ private static final String ASSET_PATH = "asset/";
 
             } catch (Exception e) {
                 System.err.println("Sound playback error: " + fileName);
+                e.printStackTrace();
             }
         }).start();
     }
 
-    /**
-     * Loads and caches clips to avoid repeated disk access.
-     */
     private static Clip getClip(String fileName)
             throws UnsupportedAudioFileException, IOException, LineUnavailableException {
 
@@ -50,6 +41,10 @@ private static final String ASSET_PATH = "asset/";
         }
 
         File soundFile = new File(ASSET_PATH + fileName);
+
+        System.out.println("Trying to load: " + soundFile.getAbsolutePath());
+        System.out.println("Exists? " + soundFile.exists());
+
         AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
 
         Clip clip = AudioSystem.getClip();
