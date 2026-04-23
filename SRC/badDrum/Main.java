787bec7729d96ebcca3bff704fea5e906a59ed6f
sam-xec <samishaikh20041507@gmail.com>
2026-04-23 17:21:44 +0100

    cleaned main file

--- SRC/badDrum/Main.java
+++ SRC/badDrum/Main.java
@@ -1,9 +1,8 @@
 package badDrum;
 
-import badDrum.sound.soundManager;
+
 
 public class Main{
 public static void main(String[] args){
-    soundManager.playSound("495662__lilmati__hi-hat.wav");
-    System.out.println("Drum project started ");
+    
 }}
