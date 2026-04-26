fd7e12d7f40e43107c777ea7fd818134d0ca07ef
Alex91R <alex.ridteygi@gmail.com>
2026-04-26 20:44:00 +0100

    puzzle logic

--- SRC/badDrum/logic/PuzzleLogic.java
+++ SRC/badDrum/logic/PuzzleLogic.java
@@ -1,5 +1,22 @@
 package badDrum.logic;
 
+import java.lang.Math;
+
 public class PuzzleLogic {
+
+private int a;
+private int b;
+private int c;
+public int x;
+
+public static generateVars(){
+    a = (int) Math.random()*21 +10;
+    b = (int) Math.random()*21 +10;
+    x = (int) Math.random()*21 +10;
+}
+
+public static calculateC(){
+    c = a*x + b;
+}
 
 }
